c5f3f06a1233c663f8c77d1d83e69f597b710129
ZhiHong Chua <chuazhihong@hotmail.com>
2025-04-04 00:23:00 +0800

    Added support for localhost debug, fix dark mode

--- index.js
+++ index.js
@@ -5,21 +5,12 @@ if (currentTheme == "dark") {
   document.body.classList.toggle("dark-theme");
 } else if (currentTheme == "light") {
   document.body.classList.toggle("light-theme");
-} else {
-  document.body.classList.toggle("light-theme");
 }
 
 btn.addEventListener("click", function () {
-    // if (prefersDarkScheme.matches) {
-    // document.body.classList.toggle("light-theme");
-    var theme = document.body.classList.contains("light-theme")
-    ? "light"
-    : "dark";
-    // } else {
-    //   document.body.classList.toggle("dark-theme");
-    //   var theme = document.body.classList.contains("dark-theme")
-    //     ? "dark"
-    //     : "light";
-    // }
+    document.body.classList.toggle("dark-theme");
+    var theme = document.body.classList.contains("dark-theme")
+      ? "dark"
+      : "light";
     localStorage.setItem("theme", theme);
   });
